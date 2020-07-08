302ba04d338f4cd9dd7436bb3cab373014534db9
Shafaat Hussain <sshabdali@hotmail.com>
2020-07-08 23:49:15 +0100

    fixed linting error

--- src/app/blockchain/pipe/trim.pipe.spec.ts
+++ src/app/blockchain/pipe/trim.pipe.spec.ts
@@ -1,7 +1,7 @@
-import { Trim } from "./trim.pipe";
+import { Trim } from './trim.pipe';
 
 describe('TrimPipe', () => {
-    let pipe = new Trim();
+    const pipe = new Trim();
 
     it('should transform long sender text to short text', () => {
         expect(pipe.transform('tz1YpWsMwc4gSyjtxEF3JbmN6YrGiDidaSmg')).toBe('tz....daSmg');
